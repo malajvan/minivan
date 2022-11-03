4ff74d5bfc1cc459b19712f462742e47a9e37cb5
Van Pham <vanhongpham01@gmail.com>
2022-11-02 20:35:10 -0400

    Fix About page
    Add column div for better layout

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,32 +3,39 @@ import { Link } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Picture from '../Resources/about_pic.jpg'
+import '../components/About.css'
 
 const About = () => {
   return (
     <div>
-
       <Navbar />
       <div style={{paddingTop:'90px',paddingLeft:"90px", color:"#fff",fontSize:'2.1rem',fontWeight:'900'}}>
           About Me
        </div> 
-      <div style={{position: 'absolute', paddingTop:'25px',paddingLeft:"90px",paddingRight:'700px', color:"#dddaff",fontSize:'1rem',fontWeight:'200'}}> 
-        Aside from being a Math & Computer Science student at McGill, I'm also an avid musician and music lover. I believe this fact is made obvious if you have had the time to look over my personal projects.
-         I'm also a Software Developer at McGill's DDMAL lab, responsible for maintaining and developing the backend of
-        a workflow management system for musicologists. 
-         <p>If you are interested in my music, feel free to visit my youtube channel or the music page of this website.</p>
-        <p> Currently listening to: I'm In The Mood For Love by Julie London</p>
+      <div class="row">
+        <div class="col">
+          <div style={{paddingTop:'25px',paddingLeft:"90px", color:"#dddaff",fontSize:'1rem',fontWeight:'500'}}> 
+                Aside from being a Math & Computer Science student at McGill, I'm also an avid musician and music lover. I believe this fact is made obvious if you have had the time to look over my personal projects.
+                I'm also a Software Developer at McGill's DDMAL lab, responsible for maintaining and developing the backend of
+                a workflow management system for musicologists. 
+                <p>If you are interested in my music, feel free to visit my youtube channel or the music page.</p>
+            </div>
         </div>
-        <div style={{position:'absolute',paddingLeft:'900px', paddingTop:'0px'}}>
-          <img style={{flex:2}} width='300' src={Picture} alt='about_portrait'/>
+        
+        <div class="col">
+          <div style={{position:'absolute',paddingLeft:'100px', paddingTop:'0px'}}>
+              <img style={{flex:2}} width='300' src={Picture} alt='about_portrait'/>
+            </div>
         </div>
 
-        
-        <div style={{paddingTop:'50px',paddingLeft:'90px'}}>
+
+          
+      </div>
+        <div style={{paddingTop:'100px',paddingLeft:'90px'}}>
           <Link to="#" className='btn' onClick={() => window.location ='mailto:vanhongpham01@gmail.com'}>Say Hello!</Link>
         </div>
         <div><Footer /></div>
-        
+      
 
     </div>
 
