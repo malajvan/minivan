05fead21c28b265662f86bf6a4146fc26be6b2e4
Van Pham <vanhongpham01@gmail.com>
2022-11-02 21:38:09 -0400

    Update Projects and Music

--- src/routes/Music.js
+++ src/routes/Music.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+
 
 const Music = () => {
   return (
-    <div>Music</div>
-  )
+    <div>
+      <Navbar />
+      <div style={{paddingTop:'25px',float:"left","width":"60%",paddingLeft:"90px", color:"#dddaff",fontSize:'1.3rem',fontWeight:'500'}}> 
+          This is currently under construction, feel free to visit my Youtube Channel in the mean time :D
+      </div>
+    </div>)
+    
 }
-
 export default Music
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Picture from '../Resources/DDMAL.png'
 
-const Projects = () => {
+
+const About = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <Navbar />
+      <div style={{paddingTop:'90px',paddingLeft:"90px", color:"#fff",fontSize:'2.1rem',fontWeight:'900'}}>
+          Projects
+       </div> 
+      <div class="row">
+        <div class="col">
+          <div style={{paddingTop:'25px',paddingLeft:"90px", color:"#dddaff",fontSize:'1rem',fontWeight:'500'}}> 
+                All of my personal projects are neatly organised in my Github page. I'm particularly proud of my work with McGill's <a style={{fontSize:"1rem", textDecoration:"underline"}} href="https://ddmal.music.mcgill.ca/" target="_blank">DDMAL lab</a> on Rodan, the workflow management system for musicologists. 
+                More information <a style={{fontSize:"1rem", textDecoration:"underline"}} href="https://github.com/DDMAL/Rodan" target="_blank">here</a>.
+            </div>
+            </div>
+        
+        <div class="col">
+          <img style={{flex:5, paddingLeft:"50px"}} width='500' src={Picture} alt='DDMAL lab picture'/>
+        </div>
+      </div>
+        <div style={{paddingTop:'10px',paddingLeft:'90px'}}>
+          <Link to="#" className='btn' onClick={() => window.location ='mailto:vanhongpham01@gmail.com'}>Say Hello!</Link>
+          <div><Footer /></div>
+        </div>
+        
+      
+
+    </div>
+
   )
 }
 
-export default Projects
+export default About
