3256bcc9f1d14bd1729d83b6b6a7bb8dc2690259
Van Pham <vanhongpham01@gmail.com>
2023-07-08 23:37:33 -0400

    add footer in Music

--- src/routes/Music.js
+++ src/routes/Music.js
@@ -12,6 +12,12 @@ const Music = () => {
       <div style={{paddingTop:'25px',float:"left","width":"60%",paddingLeft:"90px", color:"#dddaff",fontSize:'1.3rem',fontWeight:'500'}}> 
           This is currently under construction, feel free to visit my Youtube Channel in the mean time :D
       </div>
+      <div class='row' style={{paddingTop:'150px' }}>
+        <div style={{paddingTop:'10px',paddingLeft:'90px'}}>
+          <Link to="#" className='btn' onClick={() => window.location ='mailto:vanhongpham01@gmail.com'}>Say Hello!</Link>
+          <div><Footer /></div>
+        </div>
+        </div>
     </div>)
     
 }
